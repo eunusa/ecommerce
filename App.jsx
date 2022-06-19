f8b6c835d138c09d5b4e78eae5082d07ce01e654
saeunwoo <66991444+eunusa@users.noreply.github.com>
2022-06-19 23:49:35 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/Home';
+import Product from './pages/Product';
+
+function App() {
+  return (
+    <Product />
+  );
+}
+
+export default App;
